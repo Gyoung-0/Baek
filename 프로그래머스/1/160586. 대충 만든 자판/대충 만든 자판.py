317e4a83e6059d9f0f99055b1742f4bab3a8b89b
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-02-11 00:25:47 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,27 +1,20 @@
 def solution(keymap, targets):
-    # 1. 각 문자에 대해 최소 입력 횟수 저장하는 딕셔너리 생성
     key_dict = {}
-
+    answer = []
     for key_idx, key in enumerate(keymap):
-        for press_count, char in enumerate(key, start=1):  # 1부터 시작
+        for press_count, char in enumerate(key , start=1):
             if char in key_dict:
-                key_dict[char] = min(key_dict[char], press_count)  # 최소 입력 횟수 갱신
+                key_dict[char] = min(key_dict[char], press_count)
             else:
                 key_dict[char] = press_count
-
-    answer = []
-    
-    # 2. 각 target 단어에 대해 최소 입력 횟수 계산
+        
     for target in targets:
-        total_presses = 0  # 해당 단어를 입력하는 총 횟수
-        
-        for char in target:
-            if char in key_dict:
-                total_presses += key_dict[char]  # 해당 문자의 최소 입력 횟수를 더함
-            else:
-                total_presses = -1  # 하나라도 입력 불가능한 문자가 있으면 -1
+        total = 0
+        for t in target:
+            if not t in key_dict:
+                total = -1
                 break
-        
-        answer.append(total_presses)  # 결과 저장
-
+            else:
+                total += key_dict[t]
+        answer.append(total)
     return answer

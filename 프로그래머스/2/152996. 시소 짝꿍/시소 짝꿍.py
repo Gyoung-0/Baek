f4dd08e01202d356f934e86267fe48d18dd70171
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-10-01 13:40:18 +0900

    [level 2] Title: 시소 짝꿍, Time: 8.75 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -4,12 +4,10 @@ def solution(weights):
     cnt = Counter(weights)
     
     for w in cnt:
-        # 같은 무게
         if cnt[w] > 1:
-            answer += cnt[w] * (cnt[w]-1) // 2
+            answer +=  cnt[w] * (cnt[w]-1) // 2
         
-        
-        for x,y in [(2,3), (2,4), (3,4)]:
+        for x, y in ([2,3], (2,4), (3,4)):
             target = w / x * y
             if target in cnt:
                 answer += cnt[w] * cnt[target]

a58b058502ec851752f942f43b4116773918c3a8
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-10-29 15:33:57 +0900

    [level 2] Title: 올바른 괄호, Time: 9.58 ms, Memory: 9.42 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = True
+    stack = []
+
+    for i in range(len(s)):
+        
+        if s[i] == '(':
+            stack.append(s[i])
+        else:
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+
+    if stack:
+        return False
+    return True

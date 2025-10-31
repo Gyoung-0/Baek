7251ecac84d3355a50473272c067eb06576133c2
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-10-31 14:01:38 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.21 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,14 @@
+def solution(n):
+    mod = 1000000007
+    if n == 1:
+        return 1
+    dp = [0] * (n + 1)
+    
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % mod
+        
+    
+    return dp[n]

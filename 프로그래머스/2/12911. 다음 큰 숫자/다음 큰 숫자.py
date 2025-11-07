910b41d73c64da1d77f43d1a38ba4e25714835a5
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-11-07 12:00:32 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -3,21 +3,20 @@ from typing import List
 def solution(n):
     bin_n = list('0' + bin(n)[2:])
     idx = -1
-    for i in range(len(bin_n) - 1, 0, -1):
-        if bin_n[i-1] == '0' and bin_n[i] == '1':
-            idx = i - 1
+    for i in range(len(bin_n)-1, 0, -1):
+        if bin_n[i] == '1' and bin_n[i-1] == '0':
+            idx = i
+            bin_n[i-1] = '1'
+            bin_n[i] = '0'
             break
-            
-    bin_n[idx] = '1'
-    bin_n[idx+1] = '0'
-    
-    right_bin = bin_n[idx+2:]
+    right_bin = bin_n[idx+1:]
     right_count = right_bin.count('1')
     
-    new_right_bin = list('0' * (len(right_bin)-right_count) + '1' * right_count)
+    new_right_bin = list('0' * (len(right_bin) - right_count) + '1' * right_count)
     
-    final_bin_list = bin_n[:idx+2] + new_right_bin
+    final_bin = bin_n[:idx+1] + new_right_bin
+    final_str_bin = "".join(final_bin)
+    return int(final_str_bin, 2)
     
-    final_bin_str = "".join(final_bin_list)
-    return int(final_bin_str, 2)
-                        
+    
+    

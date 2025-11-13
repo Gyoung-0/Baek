a20cb71d3e24c6a0d30b421cf546d75ce09f39c8
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-11-13 22:54:19 +0900

    [level 2] Title: 미로 탈출, Time: 17.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 9.95 MB, 시간: 21.48 ms
+메모리: 10.1 MB, 시간: 17.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 24일 19:00:45
+2025년 11월 13일 22:54:14
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,53 +1,46 @@
 from collections import deque
-
 def solution(maps):
-    n, m = len(maps), len(maps[0])
-
-    # 1) 시작점 찾기
-    start = find_start(maps)
-    if start is None:
-        return -1
-    sr, sc = start
-
-    # 2) visited[r][c][lever] : lever=0(안당김), 1(당김)
-    visited = [[[False]*2 for _ in range(m)] for __ in range(n)]
+    # target의 위치를 반환하는 함수
+    def find_position(maps, target):
+        for i in range(len(maps)):
+            for j in range(len(maps[0])):
+                if maps[i][j] == target:
+                    return i, j
+    # 최단 거리는 BFS
+    
+    n, m = len(maps), len(maps[0]) # 행렬
+    
+    start_n, start_m = find_position(maps, 'S')
+    
+    # 이동 방향
+    directions = (-1, 0), (1, 0), (0, -1), (0, 1)
+    
+    # 방문한 곳은 재방문 하지 않음
+    # 레버가 당겨졌을때 안땡겨졌을때 구분해야함
+    visited = [[[False] * 2 for _ in range(m)] for _ in range(n)]
+    
     q = deque()
-    q.append((sr, sc, 0, 0))  # (r, c, lever, dist)
-    visited[sr][sc][0] = True
-
-    # 3) 상하좌우
-    dirs = [(-1,0), (1,0), (0,-1), (0,1)]
-
-    # 4) BFS
+    q.append((start_n, start_m, False, 0)) # 시작 위치 n, m , 레버 유무, 이동 거리
+    visited[start_n][start_m][0] = True
+    
     while q:
-        r, c, lever, dist = q.popleft()
-
-        # 레버를 당긴 상태에서만 E 도착을 인정
-        if maps[r][c] == 'E' and lever == 1:
+        currnet_n, current_m, lever, dist = q.popleft()
+        if lever == 1 and maps[currnet_n][current_m] == 'E':
             return dist
-
-        for dr, dc in dirs:
-            nr, nc = r + dr, c + dc
-            # 범위 체크 먼저
-            if not (0 <= nr < n and 0 <= nc < m):
+        for dn, dm in directions:
+            next_n, next_m = dn+currnet_n, dm+current_m
+            if not (0<=next_n<n and 0<=next_m<m):
                 continue
-            # 벽 제외
-            if maps[nr][nc] == 'X':
+            if visited[next_n][next_m][lever]:
                 continue
+            if maps[next_n][next_m] == 'X':
+                continue
+            
+            if maps[next_n][next_m] == 'L':    
+                q.append((next_n, next_m, 1, dist+1))
+                visited[next_n][next_m][1] = True
+                continue
+            q.append((next_n, next_m, lever, dist+1))
+            visited[next_n][next_m][lever] = True
 
-            nlever = lever
-            # 레버 발견 시 상태 전환
-            if maps[nr][nc] == 'L':
-                nlever = 1
-
-            if not visited[nr][nc][nlever]:
-                visited[nr][nc][nlever] = True
-                q.append((nr, nc, nlever, dist + 1))
     return -1
-
-def find_start(maps):
-    for i, row in enumerate(maps):
-        for j, cell in enumerate(row):
-            if cell == 'S':
-                return (i, j)
-    return None

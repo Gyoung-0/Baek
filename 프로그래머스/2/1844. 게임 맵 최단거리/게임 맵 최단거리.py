eea9150bf22620c11408a7f30df564a0b6bc1796
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-11-13 17:56:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.08 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,32 +1,32 @@
 from collections import deque
 def solution(maps):
-    answer = 0
-    n, m = len(maps), len(maps[0])
-    visited = [[False] * m for _ in range(n)] 
+    # 최단거리 BFS
+    n, m = len(maps), len(maps[0]) # 행렬
+    # 방문한 곳은 다시 가지 않게
+    visited = [[False] * m for _ in range(n)]
+    
+    # 이동방향
+    directions = (1, 0), (-1, 0), (0, 1), (0, -1)
     
     q = deque()
-    q.append((0, 0, 1))
-    visited[0][0] = True
-    dist = 1
-    directions = (-1, 0), (1, 0), (0, -1), (0, 1)
+    q.append((0,0,1)) # 출발 행, 출발 열, 이동 거리
+    visited[0][0] = True # 처음 출발 위치는 방문했음
     
     while q:
-        cn, cm, dist = q.popleft()
-        if cn == n-1 and cm == m-1:
+        current_n, current_m, dist = q.popleft()
+        if current_n == n-1 and current_m == m-1:
             return dist
-        for dx, dy in directions:
-            cdist = dist    
-            nx, ny = dx+cn, dy+cm
-            
-            if not (0<=nx<n and 0<=ny<m):
+        for dn, dm in directions:
+            next_n, next_m = current_n + dn, current_m + dm
+            if not (0 <= next_n < n and 0 <= next_m < m):
                 continue
-            if visited[nx][ny] == True:
+            if visited[next_n][next_m] == True:
                 continue
-            if maps[nx][ny] == 0:
+            if maps[next_n][next_m] == 0:
                 continue
+            current_dist = dist
             
-            q.append((nx, ny, cdist+1))
-            visited[nx][ny] = True
-            
-            
+            q.append((next_n, next_m, dist+1))
+            visited[next_n][next_m] = True
+        print(dist)
     return -1

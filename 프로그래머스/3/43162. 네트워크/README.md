785aa58fb89137277ee6418679ba10b1a823064c
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-11-14 09:45:12 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 0.68 ms
+메모리: 9.3 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 00:58:34
+2025년 11월 14일 09:45:07
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,19 @@
+from collections import deque
 def solution(n, computers):
     answer = 0
-    # 방문했는지, 1번에서 연결된거 다 가고 방문한거 표시, 해당 인덱스 배열은 스킵
     visited = [False] * n
-    # 재귀 돌면서 같은 것들 다 True로 바꿈
-    def recursive(current):
-        visited[current] = True
-        
-        for neighbor in range(n):
-            if computers[current][neighbor] == 1 and not visited[neighbor]:
-                recursive(neighbor)
+    # 해당 노드에 있는 인접한 노드들을 큐에 넣으면서 연결된 모든 노드를 변환 (양방향)
+    q = deque()
     
     for i in range(n):
-        if visited[i]:
-            continue
-        answer += 1
-        recursive(i)
         
+        if not visited[i]:
+            answer += 1
+            q.append(computers[i])
+            visited[i] = True
+            while q:
+                for idx, node in enumerate(q.popleft()):
+                    if not visited[idx] and node == 1:
+                        q.append(computers[idx])
+                        visited[idx] = True
     return answer

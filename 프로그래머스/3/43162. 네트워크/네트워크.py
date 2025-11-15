8a4e06e0347fcbd72cc706e7107b009c4b7cfa1d
Gyoung-0 <126968510+Gyoung-0@users.noreply.github.com>
2025-11-15 12:12:45 +0900

    [level 3] Title: 네트워크, Time: 0.97 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,22 @@
-from collections import deque
-def solution(n, computers):
+from typing import List
+def solution(n: int, computers: List[List]) -> int:
     answer = 0
-    visited = [False] * n
-    # 해당 노드에 있는 인접한 노드들을 큐에 넣으면서 연결된 모든 노드를 변환 (양방향)
-    q = deque()
+    # 모두 다 한번씩 돌아봐야 하니깐 dfs로
     
+    # 방문했는지 확인해야함
+    visited: bool = [False] * n
+
+    # 첫번째부터 돌면서 해당 인덱스를 제외한 인덱스의 값이 1이라면 그 인덱스 위치는 연결된걸로 봄
+    def dfs(computer: int):
+        nonlocal answer
+        for i in range(n):
+            if computers[computer][i] == 1 and not visited[i]:
+                visited[i] = True
+                dfs(i)
+                print(visited)
+        return 0
     for i in range(n):
-        
         if not visited[i]:
+            dfs(i)
             answer += 1
-            q.append(computers[i])
-            visited[i] = True
-            while q:
-                for idx, node in enumerate(q.popleft()):
-                    if not visited[idx] and node == 1:
-                        q.append(computers[idx])
-                        visited[idx] = True
     return answer
